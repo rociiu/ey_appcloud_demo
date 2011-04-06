d3fc679880117fc9974f0ee7d3ed6f23fc5b34ff
rociiu <rociiu.yu@gmail.com>
2011-04-06 16:25:04 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EyAppcloudDemo::Application.config.secret_token = '0a937bb902579e42a80e2ebb2d048a1b0f18db0bfb1cf66382e29a90d300b4470923bb67df05a95822df7dea661a6cbc3ce2f4b0b411d9c6ec6b68cf5643118c'
